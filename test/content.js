25f479160b1be228d5de063d7aac630eba354165
Fabio Forni <fabio@redaril.me>
2019-10-05 12:15:48 +0200

    ESLint: avoid escapes in strings

--- test/content.js
+++ test/content.js
@@ -1,19 +1,19 @@
 import * as content from "../reaser/content"
 
-test("<form role=\"search\"> is search element", () => {
+test('<form role="search"> is search element', () => {
 	const form = document.createElement("form")
 	form.setAttribute("role", "search")
 	expect(content.isSearchElement(form)).toBeTruthy()
 })
 
-test("<input type=\"search\"> is search element", () => {
+test('<input type="search"> is search element', () => {
 	const input = document.createElement("input")
 	input.setAttribute("type", "search")
 	expect(content.isSearchElement(input)).toBeTruthy()
 })
 
-test("input in <form type=\"search\"> is search element", () => {
-	const domparser = new DOMParser()
-	const doc = domparser.parseFromString("<form role=\"search\"><input id=\"mockInput\"></form>", "text/html")
+test('input in <form type="search"> is search element', () => {
+	const domParser = new DOMParser()
+	const doc = domParser.parseFromString('<form role="search"><input id="mockInput"></form>', "text/html")
 	expect(content.isSearchElement(doc.getElementById("mockInput"))).toBeTruthy()
 })
