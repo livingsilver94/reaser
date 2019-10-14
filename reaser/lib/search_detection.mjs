fffb19f9e3e21a63bc1425df00401dbdf8aeef7f
Fabio Forni <fabio@redaril.me>
2019-10-14 14:01:43 +0200

    Better parent form detection for search elements

--- reaser/lib/search_detection.mjs
+++ reaser/lib/search_detection.mjs
@@ -1,14 +1,21 @@
-// Web pages can define search-purposed elements in infinite ways. The lack
-// of a standard procedure to do so makes us, sometimes, not to detect
-// elements that should be detected. Fortunately there are a bunch of common
-// practices that will limit the amount of false negatives.
+/* Web pages can define search-purposed elements in infinite ways. The lack
+of a standard procedure to do so makes us, sometimes, not to detect
+elements that should be detected. Fortunately there are a bunch of common
+practices that will limit the amount of false negatives. */
 
 var wasSearching = false
 
+function attributes(element, attributes) {
+	return attributes.map(attr => element.getAttribute(attr))
+}
+
 export function isSearchElement(el) {
 	if (!el) return false
-	if (el.getAttribute("type") === "search" || el.getAttribute("role") === "search") return true
-	return isSearchElement(el.form) || false
+	if (attributes(el, ["type", "role", "name", "id"]).includes("search")) return true
+	if (el instanceof HTMLInputElement && el.getAttribute("name") === "q") return true
+
+	const form = el.closest("form"); if (form === el) return false
+	return isSearchElement(form)
 }
 
 export async function handleFocused(element) {
