dd1409f0b927ce971ee1a0222a32d5e79443cbfd
Fabio Forni <fabio@redaril.me>
2020-06-10 22:31:52 +0200

    Convert content.js into TypeScript

--- reaser/content.ts
+++ reaser/content.ts
@@ -1,31 +1,33 @@
 import { isSearchElement } from "./lib/search_detection"
 import { trustedEvent } from "./lib/trusted_event"
 
+import { browser } from 'webextension-polyfill-ts'
+
 /**
  * Keywords of the last search.
- *
- * @type {string}
  */
-var searchKey
+let searchKey: string
 
 /**
  * Tell if the user was interacting with a search-related element.
- *
- * @type {boolean}
  */
-var wasSearching
+let wasSearching: boolean
 
-const handleFocused = trustedEvent(async evt => {
+const handleFocused = trustedEvent(async (evt: FocusEvent) => {
 	if (!wasSearching) {
 		await browser.runtime.sendMessage({ searching: true })
 		wasSearching = true
 	}
 })
 
-const handleUnfocused = trustedEvent(async evt => {
-	// Don't send any message if we still are in the same parent
-	// relatedTarget is the element that is gaining focus
-	if (evt.currentTarget.contains(evt.relatedTarget)) return
+const handleUnfocused = trustedEvent(async (evt: FocusEvent) => {
+	if (evt.currentTarget == null) { return }
+
+	// Don't send any message if we still are in the same parent.
+	// relatedTarget is the element that is gaining focus.
+	const currentTarget = evt.currentTarget as HTMLElement
+	const relatedTarget = evt.relatedTarget as HTMLElement
+	if (currentTarget.contains(relatedTarget)) { return }
 
 	if (wasSearching) {
 		await browser.runtime.sendMessage({ searching: false })
@@ -33,13 +35,14 @@ const handleUnfocused = trustedEvent(async evt => {
 	}
 })
 
-const handleChangedValue = trustedEvent(evt => {
+const handleChangedValue = trustedEvent((evt: InputEvent) => {
 	if (evt.target instanceof HTMLInputElement) {
 		const type = evt.target.getAttribute("type")
-		if (type === "text" || type === "search") searchKey = evt.target.value
+		if (type == "text" || type == "search") {
+			searchKey = evt.target.value
+		}
 	}
 })
-
 
 const srcElements = Array.from(document.querySelectorAll("form, input")).filter(el => isSearchElement(el))
 for (const element of srcElements) {
@@ -54,4 +57,5 @@ browser.runtime.onMessage.addListener(request => {
 	if (request.searchStr) {
 		return Promise.resolve(searchKey)
 	}
+	return Promise.resolve()
 })
